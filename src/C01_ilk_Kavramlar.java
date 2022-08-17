d4862878e3b9582d76e03db1bf49b7298dad5068
Lokman Bayraktar <lkmnbyrktr@gmail.com>
2022-08-17 14:22:05 +0300

    ccc

--- src/C01_ilk_Kavramlar.java
+++ src/C01_ilk_Kavramlar.java
@@ -12,6 +12,23 @@ public class C01_ilk_Kavramlar {
 
         System.out.println("projede degisik yaptik yeni commit olusturalim");
         // git commit -a -m "message"
+        /*branch 'master' set up to track 'origin/master'.
+PS C:\GitGitHubTesting> git commit -a -m "bbb"
+[master 41fee02] bbb
+ 1 file changed, 9 insertions(+)
+
+ ve bbb commiti github u push edelim
+
+ git push
+
+ remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/LokmanB81/githubTseting.git
+   cfe3a87..41fee02  master -> master
+
+*/
+        // git branch ile branch olusturalım
+        // git branch ..branch adı...
+
 
     }
 }
