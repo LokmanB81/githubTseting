cfe3a8717b9863649bb1f8adff6e35a9534f2c13
Lokman Bayraktar <lkmnbyrktr@gmail.com>
2022-08-17 13:03:59 +0300

    aaa

---
+++ src/C01_ilk_Kavramlar.java
@@ -0,0 +1,8 @@
+public class C01_ilk_Kavramlar {
+
+    public static void main(String[] args) {
+
+        System.out.println("git init ile git repo olusturuldu");
+        System.out.println("git add . ile proje working den stage alındı");
+    }
+}
