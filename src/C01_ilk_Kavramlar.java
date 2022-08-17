41fee0212f53c5b1fc4f5cc23a0bba30cab01ff0
Lokman Bayraktar <lkmnbyrktr@gmail.com>
2022-08-17 13:19:39 +0300

    bbb

--- src/C01_ilk_Kavramlar.java
+++ src/C01_ilk_Kavramlar.java
@@ -4,5 +4,14 @@ public class C01_ilk_Kavramlar {
 
         System.out.println("git init ile git repo olusturuldu");
         System.out.println("git add . ile proje working den stage alındı");
+        System.out.println("git commit -m 'message' ile projenin commiti olusturuldu"); // commit aaa
+
+        // github a push yapalım
+        // git remote add origin https://github.com/LokmanB81/githubTseting.git
+        //git push -u origin main
+
+        System.out.println("projede degisik yaptik yeni commit olusturalim");
+        // git commit -a -m "message"
+
     }
 }
